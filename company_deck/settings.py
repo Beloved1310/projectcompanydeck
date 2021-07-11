f7fc6c9f083b1a6901dad815953d53152c8b8628
Beloved1310 <adeoluwafisayomi@gmail.com>
2021-07-11 14:17:20 +0100

    application commit

--- company_deck/settings.py
+++ company_deck/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = 'company/static/'
 STATICFILES_DIRS = [
-os.path.join(BASE_DIR, "static")
+os.path.join(BASE_DIR, "company/static")
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
